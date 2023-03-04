e26307742a8e6cea03d7286857d10d9b39001f62
ahmedraed101 <araed2217@gmail.com>
2023-03-04 18:04:17 +0200

    remove useless code

--- terminal_draw/pyramid_draw.c
+++ terminal_draw/pyramid_draw.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-/*
-int main(void)
-{
-	char firstname[25];
-	char lastname[25];
 
-	printf("enter your name here please: ");
-	// scanf("%s %s", firstname, lastname);
-	scanf("%[^\n]s", firstname);
-	printf("Hello %s\n", firstname);
-	return 0;
-} */
-
-// global char block, back
 
 char block;
 char back;
@@ -22,14 +9,6 @@ char forground[10];
 
 void draw_pyramid(int height);
 void pyramid_block(int height);
-void color_red();
-void color_white();
-void color_cyan();
-void color_purple();
-void color_blue();
-void color_yellow();
-void color_green();
-void color_black();
 void color_reset();
 
 int main(void){
@@ -37,6 +16,7 @@ int main(void){
 	int height;
 	char signs[21] = {'-', '+', '=', '_', '*', '&', '^', '%', '$', '#', '@', '!', ';', '/', '\\', '?', ',', '<', '>', '.', ' '};
 	char fullBlock[] = "\u2588";
+	// more in here: https://www.w3schools.com/charsets/ref_utf_block.asp
 	char colors[8][10] = {
 		"\033[0;30m", "\033[0;31m", "\033[0;32m", "\033[0;33m", "\033[0;34m", "\033[0;35m", "\033[0;36m", "\033[0;37m" 
 	};
@@ -106,65 +86,6 @@ void draw_pyramid(int height){
 	color_reset();
 }
 
-void pyramid_block(int height)
-{
-	char block[] = "\u2588";
-	char back[] = "\u2591";
-	int i, j, k, n;
-
-	k = 1;
-	for (i = 1; i <= height; i++){
-		for(j = 0; j < height - i; j++){
-			printf("%s", back);
-		}
-		color_yellow();
-		for (n = 0; n < k; n++)
-		{
-			printf("%s", block);
-		}
-		color_white();
-		for (j = 0; j < height -i; j++)
-		{
-			printf("%s", back);
-		}
-		printf("\n");
-		k += 2;
-	
-	}
-}
-
-
-void color_red(){
-	printf("\033[0;31m");
-}
-
-void color_black(){
-	printf("\033[0;30m");
-}
-
-void color_green(){
-	printf("\033[0;32m");
-}
-
-void color_yellow(){
-	printf("\033[0;33m");
-}
-
-void color_blue(){
-	printf("\033[0;34m");
-}
-
-void color_purple(){
-	printf("\033[0;35m");
-}
-
-void color_cyan(){
-	printf("\033[0;36m");
-}
-
-void color_white(){
-	printf("\033[0;37m");
-}
 
 void color_reset(){
 	printf("\033[0m");
